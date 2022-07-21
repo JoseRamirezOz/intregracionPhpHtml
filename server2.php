01a2e75c89990ee8204311c1614e22cacdaf4990
JoseRamirezOz <joe.broken@protonmail.com>
2022-07-21 13:32:57 -0500

    21/07

---
+++ server2.php
@@ -0,0 +1,10 @@
+<?php 
+// if(isset ($_POST["nombre"]) && !empty($_POST["nombre"])){
+//     echo "Si esta seteada y con un valor";
+// } else{
+//     echo "No se encontro nada";
+// }
+
+if(isset($_POST["form"])){
+    echo "Se envio el formulario";
+}
